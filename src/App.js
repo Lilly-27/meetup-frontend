b9eaf1b81a489b31dd073cdc3f3cceb072a3fca6
Lilly-27 <ghcecilia1@gmail.com>
2022-06-01 20:34:16 -0500

    merging and needing to keep my work.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-
+import "./App.css";
 import EventDisplay from './components/EventDisplay';
 import EventHomepage from './components/Homepage/EventHomepage';
 import EventDetails from './components/EventDetails';
@@ -10,19 +10,19 @@ import Show from './components/Show';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Socials from './components/Socials';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
     <div className="App">
           
-
           <Routes >
           <Route exact path='/' element={<Show/>}/>
           <Route exact path='/events' element={<EventHomepage/>}/>
           <Route exact path='/events/display' element ={<EventDisplay/>}/>
           <Route exact path='/signup' element={<SignUp/>}/>
           <Route exact path='/login' element={<Login/>}/>
-
+          <Route exact path='/user' element={<UserForm/>}/>
           <Route exact path='/socials' element={<Socials/>}/>
 
 
---
+++ src/components/SignUpForm/Success.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Success() {
+  return (
+    <div className="finalstep-container">
+        <h2>You're in!</h2>
+        <h4>Your account is complete</h4>
+    </div>
+  )
+}
+
+export default Success
